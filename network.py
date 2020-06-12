2c050a2d17a0558911ca112c705aecc2e0b95998
Tony Misic <tony.misic@ryerson.ca>
2020-06-12 19:44:33 -0400

    Still working on Backprop

--- network.py
+++ network.py
@@ -2,19 +2,26 @@ from node import Node
 import numpy as np
 from math import sqrt, exp, log
 # globals
-inputs, hidden, output = [], [], [] 
+inputs, hidden, output = [], [], []
 # hyper-parameters
-intial_bias = 0.01
-learning_rate = 0.1
+intial_bias = 0.1
+learning_rate = 0.2
 # dot product
 def dot(v1, v2):
     return sum([i*j for (i,j) in zip(v1,v2)])
 # rectified linear unit (ReLU)
 def relu(value):
     return max(0, value)
+def relu_derivative(value):
+    if value <= 0:
+        return 0
+    else:
+        return 1
 # sigmoid activation function
 def sigmoid(value):
     return exp(value) / (exp(value) + 1)
+def sigmoid_derivative(value):
+    return sigmoid(value) * (1 - sigmoid(value))
 # softmax, returns normalized list of floats
 def softmax(L):
     result = []
@@ -33,6 +40,11 @@ def rmse(Y):
     else:
         raise Exception("Output not set, incorrect output size of: " + str(len(Y)))
     return -sum(temp)
+# Square Error function
+def error(truth, predicted):
+    return 0.5 * (predicted - truth) ** 2
+def error_derivative(truth, predicted):
+    return predicted - truth
 
 # populates network with empty nodes
 def create_network(nInputs, nHidden, nOutputs):
@@ -70,23 +82,67 @@ def forward_pass(X):
             new_value = 0
             for k in layers[i]: # get current layers nodes and calculate value of next layer's node
                 new_value += k.value * k.weights[node_count]
-            j.value = relu(new_value + j.bias) # add bias and relu
+            j.value = sigmoid(new_value + j.bias) # add bias and relu
             node_count += 1
-    print("Forward Pass Complete!")
+    return output[0].value
 
 def back_propagate(Y):
     # calculate loss
-    pass
-    # update weights and biases
+    costs = []
+    costs_derivatives = []
+    if len(Y) == len(output):
+        for n in range(len(output)):
+            costs.append(error(Y[n], output[n].value))
+            costs_derivatives.append(error_derivative(Y[n], output[n].value))
+    else:
+        raise Exception("Output not set, incorrect output size of: " + str(len(Y)))
+    layers = []
+    layers.append(inputs)
+    for i in hidden:
+        layers.append(i)
+    layers.append(output)
+    # # neuron based changes hidden to output
+    # delta_h_b = sigmoid_derivative(dot([hidden[0][0].weights[0]], [hidden[0][0].value]) + output[0].bias) * costs_derivatives[0]
+    # delta_h1_w = hidden[0][0].value * sigmoid_derivative(dot([hidden[0][0].weights[0]], [hidden[0][0].value]) + output[0].bias) * costs_derivatives[0] 
+    # delta_h2_w = hidden[0][1].value * sigmoid_derivative(dot([hidden[0][1].weights[0]], [hidden[0][1].value]) + output[0].bias) * costs_derivatives[0]
+    # calculate deltas for each weight and bias
+    for layer in range(len(layers) - 1, 1, -1): # starting at output layer go back
+        layer_deltas = []
+        if layer == len(layers) - 1: # output layer
+            node_count = 0
+            for j in layers[layer]:
+                new_value = 0
+                for k in layers[layer - 1]:
+                    new_value += k.value * k.weights[node_count]
+                delta = sigmoid_derivative(new_value + j.bias) * costs_derivatives[0]
+                node_count += 1
+        else: # All layers but the ouput
+            node_count = 0
+            for j in layers[layer]:
+                new_value = 0
+                for k in layers[layer - 1]:
+                    new_value += k.value * k.weights[node_count]
+                delta = sigmoid_derivative(new_value + j.bias) 
+                node_count += 1
 
 def train(X, Y, epochs):
     for e in range(epochs):
-        pass
+        s = 0
+        for i in range(len(X)):
+            forward_pass(X[i])
+            back_propagate(Y[i])
+        print("Epoch " + str(e + 1))
 
+def test(sample, expected):
+    pass
 # create empty slate
 create_network(2, [2], 1)
-# one forward pass of training
+# Intialize XOR gate datasets
 X = [[0, 0], [0, 1], [1, 0], [1, 1]]
 Y = [[0], [1], [1], [0]]
-forward_pass(X[0])
-back_propagate(Y[0])
+# train on XOR
+train(X,Y, 200)
+print(forward_pass([0, 0]))
+print(forward_pass([0, 1]))
+print(forward_pass([1, 0]))
+print(forward_pass([1, 1]))
