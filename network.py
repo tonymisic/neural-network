0bec18144425ba0ab19f3dfd871173025d40678a
Tony Misic <tony.misic@ryerson.ca>
2020-06-13 18:27:20 -0400

    Rework in progress

--- network.py
+++ network.py
@@ -2,10 +2,12 @@ from node import Node
 import numpy as np
 from math import sqrt, exp, log
 # globals
-inputs, hidden, output = [], [], []
+weight_matrix = []
+bias_matrix = []
+activation_matrix = []
 # hyper-parameters
-intial_bias = 0.1
-learning_rate = 0.2
+intial_bias = 0.01
+learning_rate = 0.1
 # dot product
 def dot(v1, v2):
     return sum([i*j for (i,j) in zip(v1,v2)])
@@ -46,92 +48,53 @@ def error(truth, predicted):
 def error_derivative(truth, predicted):
     return predicted - truth
 
-# populates network with empty nodes
+# populates network with empty values using random initialization of weights
 def create_network(nInputs, nHidden, nOutputs):
-    for i in range(nInputs):
-        inputs.append(Node(0, np.random.uniform(-1, 1, size=(nHidden[0],))*sqrt(2./nInputs), 0)) # Kaiming initialization of weights
+    weight_matrix.append(np.random.uniform(-1, 1, size=(nInputs * nHidden[0],)))
+    bias_matrix.append(np.random.uniform(intial_bias, intial_bias, size=(nHidden[0],)))
+    activation_matrix.append(np.random.uniform(0, 0, size=(nInputs)))
     for i in range(len(nHidden)):
-        hidden.append([])
-        for _ in range(nHidden[i]):
-            if i + 1 == len(nHidden):
-                hidden[i].append(Node(0, np.random.uniform(-1, 1, size=(nOutputs,))*sqrt(2./nHidden[i]), intial_bias)) 
-            else:
-                hidden[i].append(Node(0, np.random.uniform(-1, 1, size=(nHidden[i+1],))*sqrt(2./nHidden[i]), intial_bias))
-    for i in range(nOutputs):
-        output.append(Node(0, [], intial_bias))
+        if i + 1 == len(nHidden):
+            weight_matrix.append(np.random.uniform(-1, 1, size=(nHidden[i] * nOutputs,)))
+            activation_matrix.append(np.random.uniform(0, 0, size=(nHidden[i])))
+        else:
+            weight_matrix.append(np.random.uniform(-1, 1, size=(nHidden[i] * nHidden[i + 1],)))
+            bias_matrix.append(np.random.uniform(intial_bias, intial_bias, size=(nHidden[i + 1],)))
+            activation_matrix.append(np.random.uniform(0, 0, size=(nHidden[i],)))
+    bias_matrix.append(np.random.uniform(intial_bias, intial_bias, size=(nOutputs,)))
+    activation_matrix.append(np.random.uniform(0, 0, size=(nOutputs,)))
     print("Network Created ... Architecture: Input - " + str(nInputs) + " Hidden - " + str(nHidden) + " Output - " + str(nOutputs))
 
 def set_input(current):
-    if len(current) == len(inputs):
-        for i in range(len(inputs)):
-            inputs[i].value = current[i]
-    else:
-        raise Exception("Input not set, incorrect input size of: " + str(len(current)))
+    activation_matrix[0] = current
 
 # performs one forward pass of the network
 def forward_pass(X):
-    layers = []
     set_input(X)
-    layers.append(inputs)
-    for i in hidden:
-        layers.append(i)
-    layers.append(output)
-    for i in range(len(layers) - 1): # each layer in the network
-        node_count = 0
-        for j in layers[i + 1]: # get next layer's nodes and set their values
-            new_value = 0
-            for k in layers[i]: # get current layers nodes and calculate value of next layer's node
-                new_value += k.value * k.weights[node_count]
-            j.value = sigmoid(new_value + j.bias) # add bias and relu
-            node_count += 1
-    return output[0].value
+    for i in range(len(activation_matrix) - 1):
+        for j in range(len(activation_matrix[i + 1])):
+            l = len(activation_matrix[i])
+            activation_matrix[i + 1][j] = dot(weight_matrix[i][], activation_matrix[i]) + bias_matrix[i][j]
+    return activation_matrix[-1][0]
 
 def back_propagate(Y):
-    # calculate loss
-    costs = []
-    costs_derivatives = []
-    if len(Y) == len(output):
-        for n in range(len(output)):
-            costs.append(error(Y[n], output[n].value))
-            costs_derivatives.append(error_derivative(Y[n], output[n].value))
-    else:
-        raise Exception("Output not set, incorrect output size of: " + str(len(Y)))
-    layers = []
-    layers.append(inputs)
-    for i in hidden:
-        layers.append(i)
-    layers.append(output)
-    # # neuron based changes hidden to output
-    # delta_h_b = sigmoid_derivative(dot([hidden[0][0].weights[0]], [hidden[0][0].value]) + output[0].bias) * costs_derivatives[0]
-    # delta_h1_w = hidden[0][0].value * sigmoid_derivative(dot([hidden[0][0].weights[0]], [hidden[0][0].value]) + output[0].bias) * costs_derivatives[0] 
-    # delta_h2_w = hidden[0][1].value * sigmoid_derivative(dot([hidden[0][1].weights[0]], [hidden[0][1].value]) + output[0].bias) * costs_derivatives[0]
-    # calculate deltas for each weight and bias
-    for layer in range(len(layers) - 1, 1, -1): # starting at output layer go back
-        layer_deltas = []
-        if layer == len(layers) - 1: # output layer
-            node_count = 0
-            for j in layers[layer]:
-                new_value = 0
-                for k in layers[layer - 1]:
-                    new_value += k.value * k.weights[node_count]
-                delta = sigmoid_derivative(new_value + j.bias) * costs_derivatives[0]
-                node_count += 1
-        else: # All layers but the ouput
-            node_count = 0
-            for j in layers[layer]:
-                new_value = 0
-                for k in layers[layer - 1]:
-                    new_value += k.value * k.weights[node_count]
-                delta = sigmoid_derivative(new_value + j.bias) 
-                node_count += 1
+    loss = error_derivative(Y[0], activation_matrix[-1][0])
+
+    return loss
 
 def train(X, Y, epochs):
     for e in range(epochs):
         s = 0
         for i in range(len(X)):
             forward_pass(X[i])
-            back_propagate(Y[i])
-        print("Epoch " + str(e + 1))
+            s += back_propagate(Y[i])
+        print("Epoch " + str(e + 1) + " Average Error: " + str(s / len(Y)))
+
+def print_network():
+    print(" - - - Network - - - ")
+    print("Weights: " + str(weight_matrix))
+    print("Activations: " + str(activation_matrix))
+    print("Biases" + str(bias_matrix))
 
 def test(sample, expected):
     pass
@@ -141,8 +104,11 @@ create_network(2, [2], 1)
 X = [[0, 0], [0, 1], [1, 0], [1, 1]]
 Y = [[0], [1], [1], [0]]
 # train on XOR
-train(X,Y, 200)
-print(forward_pass([0, 0]))
-print(forward_pass([0, 1]))
-print(forward_pass([1, 0]))
-print(forward_pass([1, 1]))
+train(X,Y, 1)
+print_network()
+# show result
+print("XOR Table Results: ")
+print(forward_pass([0,0]))
+print(forward_pass([0,1]))
+print(forward_pass([1,0]))
+print(forward_pass([1,1]))
