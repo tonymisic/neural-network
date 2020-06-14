25127431549594edaed87245e3f0d4576b56619e
Tony Misic <tony.misic@ryerson.ca>
2020-06-13 21:08:23 -0400

    Progress on backprop (matrix implementation)

--- network.py
+++ network.py
@@ -1,13 +1,13 @@
 from node import Node
 import numpy as np
-from math import sqrt, exp, log
+from math import sqrt, exp, log, floor, ceil
 # globals
-weight_matrix = []
-bias_matrix = []
-activation_matrix = []
+weights, gradient_weights = [], []
+biases, gradient_biases = [], []
+activations = []
 # hyper-parameters
 intial_bias = 0.01
-learning_rate = 0.1
+learning_rate = 0.01
 # dot product
 def dot(v1, v2):
     return sum([i*j for (i,j) in zip(v1,v2)])
@@ -36,9 +36,9 @@ def softmax(L):
 # Root Mean Square Error (RMSE)
 def rmse(Y):
     temp = []
-    if len(Y) == len(output):
-        for n in range(len(output)):
-            temp.append(Y[n] * log(output[n].value))
+    if len(Y) == len(activations[-1]):
+        for n in range(len(activations[-1])):
+            temp.append(Y[n] * log(activations[-1][n].value))
     else:
         raise Exception("Output not set, incorrect output size of: " + str(len(Y)))
     return -sum(temp)
@@ -50,65 +50,95 @@ def error_derivative(truth, predicted):
 
 # populates network with empty values using random initialization of weights
 def create_network(nInputs, nHidden, nOutputs):
-    weight_matrix.append(np.random.uniform(-1, 1, size=(nInputs * nHidden[0],)))
-    bias_matrix.append(np.random.uniform(intial_bias, intial_bias, size=(nHidden[0],)))
-    activation_matrix.append(np.random.uniform(0, 0, size=(nInputs)))
+    weights.append(np.random.uniform(-1, 1, size=(nInputs * nHidden[0],)))
+    gradient_weights.append(np.random.uniform(0, 0, size=(nInputs * nHidden[0],)))
+    biases.append(np.random.uniform(intial_bias, intial_bias, size=(nHidden[0],)))
+    gradient_biases.append(np.random.uniform(0, 0, size=(nHidden[0],)))
+    activations.append(np.random.uniform(0, 0, size=(nInputs)))
     for i in range(len(nHidden)):
         if i + 1 == len(nHidden):
-            weight_matrix.append(np.random.uniform(-1, 1, size=(nHidden[i] * nOutputs,)))
-            activation_matrix.append(np.random.uniform(0, 0, size=(nHidden[i])))
+            weights.append(np.random.uniform(-1, 1, size=(nHidden[i] * nOutputs,)))
+            gradient_weights.append(np.random.uniform(0, 0, size=(nHidden[i] * nOutputs,)))
+            activations.append(np.random.uniform(0, 0, size=(nHidden[i])))
         else:
-            weight_matrix.append(np.random.uniform(-1, 1, size=(nHidden[i] * nHidden[i + 1],)))
-            bias_matrix.append(np.random.uniform(intial_bias, intial_bias, size=(nHidden[i + 1],)))
-            activation_matrix.append(np.random.uniform(0, 0, size=(nHidden[i],)))
-    bias_matrix.append(np.random.uniform(intial_bias, intial_bias, size=(nOutputs,)))
-    activation_matrix.append(np.random.uniform(0, 0, size=(nOutputs,)))
+            weights.append(np.random.uniform(-1, 1, size=(nHidden[i] * nHidden[i + 1],)))
+            gradient_weights.append(np.random.uniform(0, 0, size=(nHidden[i] * nHidden[i + 1],)))
+            biases.append(np.random.uniform(intial_bias, intial_bias, size=(nHidden[i + 1],)))
+            gradient_biases.append(np.random.uniform(0, 0, size=(nHidden[i + 1],)))
+            activations.append(np.random.uniform(0, 0, size=(nHidden[i],)))
+    biases.append(np.random.uniform(intial_bias, intial_bias, size=(nOutputs,)))
+    gradient_biases.append(np.random.uniform(0, 0, size=(nOutputs,)))
+    activations.append(np.random.uniform(0, 0, size=(nOutputs,)))
     print("Network Created ... Architecture: Input - " + str(nInputs) + " Hidden - " + str(nHidden) + " Output - " + str(nOutputs))
 
 def set_input(current):
-    activation_matrix[0] = current
+    activations[0] = current
 
 # performs one forward pass of the network
 def forward_pass(X):
     set_input(X)
-    for i in range(len(activation_matrix) - 1):
-        for j in range(len(activation_matrix[i + 1])):
-            l = len(activation_matrix[i])
-            activation_matrix[i + 1][j] = dot(weight_matrix[i][], activation_matrix[i]) + bias_matrix[i][j]
-    return activation_matrix[-1][0]
+    for i in range(len(biases)):
+        for j in range(len(biases[i])):
+            L = len(biases[i])
+            activations[i + 1][j] = sigmoid(dot(weights[i][j*L:j*L+L], activations[i]) + biases[i][j])
+    return activations[-1][0]
 
 def back_propagate(Y):
-    loss = error_derivative(Y[0], activation_matrix[-1][0])
-
-    return loss
+    loss_der = error_derivative(Y[0], activations[-1][0])
+    # calculate gradients
+    for i in reversed(range(len(weights))):
+        if i == len(weights) - 1: # output layer
+            for j in range(len(biases[i])):
+                L = len(biases[i])
+                gradient_biases[i][j] = sigmoid_derivative(dot(weights[i][j*L:j*L+L], activations[i]) + biases[i][j]) * loss_der
+                for k in range(len(weights[i])):
+                    gradient_weights[i][k] = activations[i][j] * sigmoid_derivative(dot(weights[i][k*L:k*L+L], activations[i]) + biases[i][j]) * loss_der
+        else: # all other layers
+            for j in range(len(biases[i])): # 2 times
+                L = len(biases[i])
+                gradient_biases[i][j] = sigmoid_derivative(dot(weights[i][j*L:j*L+L], activations[i]) + biases[i][j]) * loss_der
+                for k in range(len(weights[i])): # 4 times
+                    gradient_weights[i][k] = activations[i][j] * sigmoid_derivative(dot(weights[i][k*L:k*L+L], activations[i]) + biases[i][j]) * loss_der
+    # update biases with learning rate
+    for i in range(len(gradient_biases)):
+        for j in range(len(gradient_biases[i])):
+            biases[i][j] += learning_rate * gradient_biases[i][j]
+    # update weights with learning rate
+    for i in range(len(gradient_weights)):
+        for j in range(len(gradient_weights[i])):
+            weights[i][j] += learning_rate * weights[i][j]
+    return loss_der
 
 def train(X, Y, epochs):
     for e in range(epochs):
         s = 0
         for i in range(len(X)):
             forward_pass(X[i])
-            s += back_propagate(Y[i])
-        print("Epoch " + str(e + 1) + " Average Error: " + str(s / len(Y)))
-
+            s = back_propagate(Y[i])
+        print("Epoch " + str(e + 1))
+        
 def print_network():
     print(" - - - Network - - - ")
-    print("Weights: " + str(weight_matrix))
-    print("Activations: " + str(activation_matrix))
-    print("Biases" + str(bias_matrix))
-
+    print("Weights: " + str(weights))
+    print("Gradients: " + str(gradient_weights))
+    print("Biases" + str(biases))
+    print("Gradients: " + str(gradient_biases))
+    print("Activations: " + str(activations))
+    print(" - - - Network - - - ")
 def test(sample, expected):
     pass
+
 # create empty slate
 create_network(2, [2], 1)
 # Intialize XOR gate datasets
 X = [[0, 0], [0, 1], [1, 0], [1, 1]]
 Y = [[0], [1], [1], [0]]
 # train on XOR
-train(X,Y, 1)
+train(X,Y, 100)
 print_network()
 # show result
 print("XOR Table Results: ")
-print(forward_pass([0,0]))
-print(forward_pass([0,1]))
-print(forward_pass([1,0]))
-print(forward_pass([1,1]))
+print('{:f}'.format(forward_pass([0,0])))
+print('{:f}'.format(forward_pass([0,1])))
+print('{:f}'.format(forward_pass([1,0])))
+print('{:f}'.format(forward_pass([1,1])))
